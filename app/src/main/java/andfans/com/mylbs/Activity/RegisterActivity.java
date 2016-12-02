8eddda1f7a56dd465d4780b02b21111337e91e62
fly <614015229@qq.com>
2016-12-02 17:23:26 +0800

    添加短信验证

--- app/src/main/java/andfans/com/mylbs/Activity/RegisterActivity.java
+++ app/src/main/java/andfans/com/mylbs/Activity/RegisterActivity.java
@@ -3,18 +3,23 @@ package andfans.com.mylbs.Activity;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import andfans.com.mylbs.R;
 import andfans.com.mylbs.util.Utils;
+import cn.bmob.sms.BmobSMS;
+import cn.bmob.sms.exception.BmobException;
+import cn.bmob.sms.listener.RequestSMSCodeListener;
+import cn.bmob.sms.listener.VerifySMSCodeListener;
 
 /*
  * Created by 兆鹏 on 2016/11/30.
  */
 public class RegisterActivity extends Activity {
-    private ImageView imBack;
+    private ImageView imBack,imUserPic;
     private EditText edUser,edTelephone,edBrand,edPassword,edConfirmCode;
     private Button btRegister,btGetConfirm;
     private boolean isRegister = false;
@@ -28,6 +33,7 @@ public class RegisterActivity extends Activity {
     }
 
     private void findView() {
+        imUserPic = (ImageView) findViewById(R.id.id_register_userPic);
         imBack = (ImageView) findViewById(R.id.id_register_back);
         edConfirmCode = (EditText) findViewById(R.id.id_register_confirmCode);
         edUser = (EditText) findViewById(R.id.id_register_user);
@@ -43,7 +49,37 @@ public class RegisterActivity extends Activity {
                 finish();
             }
         });
-        btRegister.setOnClickListener(view -> Utils.showToast(context,"点击了注册按钮"));
-        btGetConfirm.setOnClickListener(view -> Utils.showToast(context,"点击了获取验证码"));
+        btRegister.setOnClickListener(view -> {
+            Utils.showToast(context,"点击了注册按钮");
+            BmobSMS.verifySmsCode(context,
+                    edTelephone.getText().toString(),
+                    edConfirmCode.getText().toString(),
+                    new VerifySMSCodeListener() {
+                        @Override
+                        public void done(BmobException ex) {
+                            if(ex==null){//短信验证码已验证成功
+                                Log.i("bmob", "验证通过");
+                            }else{
+                                Log.i("bmob", "验证失败：code ="+ex.getErrorCode()+",msg = "+ex.getLocalizedMessage());
+                            }
+                        }
+                    });
+        });
+        btGetConfirm.setOnClickListener(view -> {
+            Utils.showToast(context,"点击了获取验证码");
+            BmobSMS.requestSMSCode(context,
+                    edTelephone.getText().toString(),
+                    "MyLBS",
+                    new RequestSMSCodeListener() {
+                        @Override
+                        public void done(Integer smsId, BmobException ex) {
+                            if(ex==null){//
+                                Log.i("bmob","短信发送成功，短信id："+smsId);//用于查询本次短信发送详情
+                            }else{
+                                Log.i("bmob","errorCode = "+ex.getErrorCode()+",errorMsg = "+ex.getLocalizedMessage());
+                            }
+                        }
+                    });
+        });
     }
 }
--- app/src/main/java/andfans/com/mylbs/Application/MyLBS.java
+++ app/src/main/java/andfans/com/mylbs/Application/MyLBS.java
@@ -9,7 +9,9 @@ import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
 
-/**
+import cn.bmob.sms.BmobSMS;
+
+/*
  * Created by 兆鹏 on 2016/11/25.
  */
 public class MyLBS extends Application {
@@ -19,6 +21,7 @@ public class MyLBS extends Application {
         //在使用SDK各组件之前初始化context信息，传入ApplicationContext
         //注意该方法要再setContentView方法之前实现
         super.onCreate();
+        BmobSMS.initialize(this,"58d3c7005c6ca0f14436244e2bf0f75c");
         SDKInitializer.initialize(this);
         //registerSDKCheckReceiver();
     }
