7807bc5924142d3d8a36da500dd5dd86f011a1c2
fly <614015229@qq.com>
2016-12-02 17:28:20 +0800

    添加短信验证

--- app/src/main/java/andfans/com/mylbs/util/utils.java
+++ app/src/main/java/andfans/com/mylbs/util/utils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class Utils {
+    public static boolean isRegister = false;
+    public static boolean isLoading = false;
     public static void showToast(Context context,CharSequence s){
         Toast toast = Toast.makeText(context,s,Toast.LENGTH_SHORT);
         toast.show();
